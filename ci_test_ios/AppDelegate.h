bf69d008cfb9b2a42b9a216a363a3c34533b6f91
dck28 <dck28@cornell.edu>
2015-06-03 19:07:46 +0800

    first commit

---
+++ ci_test_ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ci_test_ios
+//
+//  Created by Dennis Kong on 6/3/15.
+//  Copyright (c) 2015 ExpressVPN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ci_test_ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ci_test_ios
+//
+//  Created by Dennis Kong on 6/3/15.
+//  Copyright (c) 2015 ExpressVPN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
